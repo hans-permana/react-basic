153b79b4a3e4f95d4f079c745b23207f791c1e60
Hans <hans@agiletechnica.com>
2016-11-15 23:12:08 +0100

    add default initial state expenses

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,17 +1,32 @@
 import React from 'react';
 
+import expenses from '../data/expenses';
+
 class ExpenseList extends React.Component {
-    render() {
+    constructor() {
+        super();
+        this.state = {
+            expenses: expenses
+        };
+    }
+
+    renderExpense(expense, i) {
         return (
-            <div>
-                <p>Expense €Amount Date</p>
-                <p>Expense €Amount Date</p>
-                <p>Expense €Amount Date</p>
-                <p>Expense €Amount Date</p>
+            <div key={i}>
+                <p>
+                    <strong>{expense.description}</strong> €{expense.amount} {expense.date}
+                </p>
             </div>
         )
     }
 
+    render() {
+        return (
+            <div>
+                {this.state.expenses.map(this.renderExpense)}
+            </div>
+        )
+    }
 }
 
 export default ExpenseList;
---
+++ src/data/expenses.js
@@ -0,0 +1,19 @@
+const expenses = [
+    {
+        description: "haircut",
+        amount: "21.00",
+        date: "10-11-2016"
+    },
+    {
+        description: "lunch",
+        amount: "3.90",
+        date: "10-11-2016"
+    },
+    {
+        description: "grocery",
+        amount: "28.84",
+        date: "10-11-2016"
+    },
+];
+
+export default expenses;
