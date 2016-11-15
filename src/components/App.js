e70e13e74f6346ceefe08871051834cfefe45343
Hans <hans@agiletechnica.com>
2016-11-15 23:01:25 +0100

    add dummy expenselist

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {render} from 'react-dom';
 
 import ExpenseInput from './ExpenseInput';
+import ExpenseList from './ExpenseList';
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <ExpenseInput/>
+                <ExpenseList/>
             </div>
         )
     }
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class ExpenseList extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Expense €Amount Date</p>
+                <p>Expense €Amount Date</p>
+                <p>Expense €Amount Date</p>
+                <p>Expense €Amount Date</p>
+            </div>
+        )
+    }
+
+}
+
+export default ExpenseList;
